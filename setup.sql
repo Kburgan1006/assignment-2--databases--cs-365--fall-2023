33d904935b3fedf09c0851f5bbea5d3abda1e704
Katherine Burgan <burgankatherine@icloud.com>
2023-10-09 11:51:59 -0400

    Update setup.sql
    Add schema

---
+++ setup.sql
@@ -0,0 +1,10 @@
+CREATE TABLE passwords (
+    Website_Name VARCHAR(50),
+    Website_URL VARCHAR(2083),
+    First_Name VARCHAR(20),
+    Middle_Name VARCHAR(20),
+    Last_Name VARCHAR(20),
+    Password_ VARCHAR(64),
+    COMMENT TEXT,
+    CREATION_TIME DATETIME
+);
