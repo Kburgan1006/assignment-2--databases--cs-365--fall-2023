fafe5be0a582b88f7b5a780f470d83ff4c40488b
Katherine Burgan <burgankatherine@icloud.com>
2023-10-28 19:09:45 -0400

    Update commands and setup

--- commands.sql
+++ commands.sql
@@ -1,43 +1,20 @@
-CREATE PROCEDURE InsertEntry IN( @Website_Name VARCHAR(50), NOT NULL
-    @Website_URL VARCHAR(2083),
-    @First_Name VARCHAR(20),
-    @Last_Name VARCHAR(20),
-    @User_Name VARCHAR(20), NOT NULL
-    @Email_Address VARCHAR(320),
-    @Password_ VARCHAR(64), NOT NULL
-    @COMMENT TEXT)
-BEGIN
-INSERT INTO 
-    passwords (Website_Name, Website_URL, First_Name, Last_Name, User_Name, Email_Address, Password_, Comment)
-VALUES  
-    (@Website_Name, @Website_URL, @First_Name, @Last_Name, @User_Name, @Email_Address, @Password_, @Comment);
-END;
+-- Select all passwords associated with the given URL
+SELECT CAST(AES_DECRYPT(Password_, @key_str, @init_vector) AS CHAR(512)) FROM passwords
+WHERE Website_URL = "www.google.com";
 
-CREATE PROCEDURE GetPassword IN(@URL)
-BEGIN
-SELECT Password_ FROM passwords
-WHERE Website_URL = @URL
-END;
+-- Select all passwords with https in the URL
+SELECT * FROM passwords WHERE Website_URL LIKE '%https%';
 
-CREATE PROCEDURE GetAllHTTPS
-BEGIN
-SELECT * FROM passwords
-WHERE Website_URL LIKE '%https%'
-END;
+-- Update password associated with the old password
+UPDATE passwords
+SET Password_ = AES_ENCRYPT('hey', @key_str, @init_vector) WHERE CAST(AES_DECRYPT(Password_, @key_str, @init_vector) AS CHAR(512)) = 'yeet123!';
 
+-- Update URL associated withe old URL
+UPDATE passwords
+SET Website_URL = 'bruh' WHERE Website_URL = 'www.google.com';
 
-CREATE PROCEDURE ChangePassword IN (@OldPassword VARCHAR(64),
-@NewPassword VARCHAR(64))
-BEGIN
-SET Password_ = @NewPassword
-WHERE Password_ = @OldPassword
-END;
+-- Delete row that contains entered URL
+DELETE FROM passwords WHERE Website_URL = 'www.google.com';
 
-CREATE PROCEDURE ChangeURL IN (@OldURL VARCHAR(2083), @NewURL
-VARCHAR(2083))
-BEGIN
-SET Website_URL = @NewURL
-WHERE Website_URL = @OldURL
-END;
-
-
+-- Delete row that contains entered password
+DELETE FROM passwords WHERE Passwords_ = 'yeet123';
--- setup.sql
+++ setup.sql
@@ -1,14 +1,38 @@
+--Sets up database and tables
+DROP DATABASE IF EXISTS passwords;
+
+CREATE DATABASE passwords;
+
+USE passwords;
+
 CREATE TABLE passwords (
-    Passwords_Key  INT AUTO_INCREMENT NOT NULL
-    Website_Name VARCHAR(50), NOT NULL
+    Password_id  INT(5) NOT NULL AUTO_INCREMENT,
+    Website_Name VARCHAR(50) NOT NULL,
     Website_URL VARCHAR(2083),
     First_Name VARCHAR(20),
     Last_Name VARCHAR(20),
-    User_Name VARCHAR(20), NOT NULL
+    User_Name VARCHAR(20) NOT NULL,
     Email_Address VARCHAR(320),
-    Password_ VARCHAR(64), NOT NULL
+    Password_ VARBINARY(512) NOT NULL,
     Comment TEXT,
-    Creation_Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    Creation_Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY(Passwords_Key)
+    PRIMARY KEY(Password_id)
 );
+
+-- Sets up encryption
+SET block_encryption_mode = 'aes-256-cbc';
+SET @key_str = UNHEX(SHA2('hawks ohio squirrel', 512));
+SET @init_vector = RANDOM_BYTES(16);
+
+-- Populates table with 10 values
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
+Insert INTO passwords VALUES(NULL, 'Google.com', 'www.google.com', 'Bill', 'Bob', 'BillB02', 'billbob@gmail.com', AES_ENCRYPT('yeet123!', @key_str, @init_vector), "Google account made for school", now());
